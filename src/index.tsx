23827ea8b169e70869aa67ea112a0e38681a0a21
sabhas <sabirbk06@gmail.com>
2021-11-27 23:32:46 +0500

    feat: complete basic setup of react-seed-app from scratch

---
+++ src/index.tsx
@@ -0,0 +1,8 @@
+import React from 'react'
+import ReactDOM from 'react-dom'
+
+const HelloWorld = () => {
+  return <h1>Hello World</h1>
+}
+
+ReactDOM.render(<HelloWorld />, document.getElementById('root'))
